19ea1cceabdef0a903f59782b4642de88693a2be
Celia Rizzo <crizzo16@users.noreply.github.com>
2018-06-20 18:03:01 -0700

    Finished the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,19 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var index = Math.floor(Math.random() * alphabet.length);
 var answer = alphabet[index];
 
-console.log(answer); // checking to make sure the answer was generated
+//console.log(answer); // checking to make sure the answer was generated 
+//Debugging purposes - I'm not gonna give someone the answer if they're clever enough to look!
 
-// **** Initializing important variables
-var win = 0;
-var lose = 0;
+// **** Initializing important variables ****
+var win = 0; //counts number of wins
+var lose = 0; //counts number of losses
 var guessesLeft = 9; // Number of guesses the player has left
-var guesses = ""; // Player's guesses will get added to this string
-document.getElementById("guessesLeft").innerHTML = guessesLeft;
+var guesses = []; // Player's guesses will get added to this string
+document.getElementById("guessesLeft").innerHTML = guessesLeft; //Update screen
 
 
 document.onkeyup = function (event) {
-    // **** Get Input from User ****
+    // Get Input from User
     var input = event.key;
     if (alphabet.includes(input)) {
         //If the player guesses the letter correctly
@@ -30,9 +31,11 @@ document.onkeyup = function (event) {
             //Select new letter
             index = Math.floor(Math.random() * alphabet.length);
             answer = alphabet[index];
+            guesses = [];
         }
-        //If the user guesses incorrectly and it was their last turn
-        else if (guessesLeft === 1) {
+        //If the user guesses incorrectly and it was their last turn 
+        //Also, don't penalize players for guessing the same letter twice
+        else if (guessesLeft === 1 && !guesses.includes(input)) {
             //increment Lose
             lose = lose + 1;
             document.getElementById("losses").innerHTML = lose;
@@ -43,18 +46,17 @@ document.onkeyup = function (event) {
             //select new letter
             index = Math.floor(Math.random() * alphabet.length);
             answer = alphabet[index];
+            guesses = [];
         }
         // If the user guesses incorrectly and they can guess again
+        // Also, don't penalize players if they guess the same letter again
         else {
-            guessesLeft = guessesLeft - 1; //decrement guessesLeft
-            document.getElementById("guessesLeft").innerHTML = guessesLeft;
-            //print their guess to the screen
-            if (guessesLeft === 8) {
-                guesses = input;
-                document.getElementById("guesses").innerHTML = guesses;
-            } else {
-                guesses = guesses + ", " + input;
-                document.getElementById("guesses").innerHTML = guesses;
+            if (!guesses.includes(input)) {
+                guessesLeft = guessesLeft - 1; //decrement guessesLeft
+                document.getElementById("guessesLeft").innerHTML = guessesLeft;
+                //print their guess to the screen
+                guesses.push(input);
+                document.getElementById("guesses").innerHTML = guesses.join(", ");
             }
         }
     }
